20af7909e36f945719c7ffe62d7faacfa7124e54
Florian Gutmann <f.gutmann@chronimo.com>
2014-02-01 15:46:31 +0100

    some progress

--- web/wsfp-browser.dart
+++ web/wsfp-browser.dart
@@ -1,25 +1,72 @@
 import 'package:polymer/polymer.dart';
 import 'dart:html';
+import 'package:dartflightschool/protocol.dart' as p;
+import 'package:dartflightschool/dummyimpl.dart'; 
 
 @CustomTag("wsfp-browser")
 class WsfpBrowser extends PolymerElement {
-  
+
+  bool get applyAuthorStyles => true;
+
   @observable
   String url;
+
+  @observable
+  List<p.File> currentFiles = new List();
   
-  WsfpBrowser.created() : super.created() {
-    print("hello");
-  }
+  
+  /// the current protocol id
+  String currentProtocolId;
+  
+  /// the currently closed 
+  p.Protocol currentProtocol;
+  
+  /// initialization logic
+  WsfpBrowser.created() : super.created() {}
   
   enteredView() {
     super.enteredView();
-    print("hello2");
-    print($['wsfp-url']);
     
     $['wsfp-url'].onKeyPress.listen((KeyboardEvent event) {
       if(event.keyCode == 13) {
-        url = 'Hey, you just pressed enter';
+        list(parse(url));
       }
     });    
+  }
+  
+  /**
+   * Parses the url and returns protocol and path.
+   * If the url is not well-formed or the protocol is not found this returns null.
+   */
+  p.Protocol getProcotol(String url) {
+    var regex = new RegExp('([^:]+)://([^/]*)/(.*)');
+    var match = regex.matchAsPrefix(url);
+    if(match == null) {
+      return null;
+    }
+    
+    var type = match.group(1);
+    var host = match.group(2);
+    var path = match.group(3);
+    
+    var protocolId = type + "_" + host;
+    
+    if(currentProtocolId == protocolId) {
+      return currentProtocol;
+    }
+    
+    if (currentProtocol != null) { 
+      currentProtocol.close();
+    }
+    currentProtocolId = protocolId;
+    
+    switch(type) {
+      case "dummy":
+         currentProtocol = new DummyFs();
+    }
+  }
+  
+  void list(p.Protocol protocol, String path) {
+    
   }
 }
--- web/wsfp-rootcomponent.dart
+++ web/wsfp-rootcomponent.dart
@@ -2,6 +2,9 @@ import 'package:polymer/polymer.dart';
 
 @CustomTag("wsfp-rootcomponent")
 class WsfpRootComponent extends PolymerElement {
+  
+  bool get applyAuthorStyles => true;
+  
   WsfpRootComponent.created() : super.created() {
     print("hello");
   }
