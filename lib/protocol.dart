6835e056ef7f43b03d306959731875d14e970b32
Florian Gutmann <f.gutmann@chronimo.com>
2014-02-01 13:19:51 +0100

    first protocol draft

---
+++ lib/protocol.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'dart:io';
+
+class File {
+  /// The name of the file or directory
+  String name;
+  
+  /// Filesize in bytes or number of subfiles/directories if it is a directory.
+  int size;
+  
+  /// If the file is a directory or file
+  bool isDirectory;
+  
+  File(this.name, this.isDirectory, this.size);
+}
+
+abstract class Protocol {
+  /**
+   *  List the files and directories at the given path.
+   *  Returns null if the given path is not a readable directory.
+   */
+  List<File> list(String path);
+  
+  /**
+   * Opens the given path for writing.
+   */
+  // TODO: change type of IOSink, must be stream or something other
+  // IOSink is defined in dart:io which won't work in browsers
+  IOSink openWrite(String path);
+  
+  /**
+   * Opens the given path for reading.
+   */
+  Stream<List<int>> open(String path);
+}
